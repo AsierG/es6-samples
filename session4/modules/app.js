30373045f3358eedbc105d504f0460645725fcaa
root <root@DESKTOP-K035CVK.localdomain>
2017-08-08 16:21:34 +0200

    session4

---
+++ session4/modules/app.js
@@ -0,0 +1,5 @@
+import {suma} from './suma';
+import Rectangle from './rectangle';
+
+console.log(suma(2, 2));
+console.log(Rectangle);
---
+++ session4/modules/miModulo.js
@@ -0,0 +1,10 @@
+export var color = 'red';
+export let name = 'Asier';
+export const MAGIC_NUMBER = 8;
+
+import MiModulo from './miModulo'
+
+miModulo.color;
+miModulo.name;
+miModulo.MAGIC_NUMBER;
+
