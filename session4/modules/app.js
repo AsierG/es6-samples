ec389a7d581c3de957737c09d9918c96e23417f3
AsierG <asiergrc@gmail.com>
2017-08-10 19:02:06 +0200

    session5

--- session4/modules/app.js
+++ session4/modules/app.js
@@ -1,5 +1,6 @@
-import {suma} from './suma';
+import {suma, resta} from './suma';
 import Rectangle from './rectangle';
 
 console.log(suma(2, 2));
+console.log(resta(5, 2));
 console.log(Rectangle);
